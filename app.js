b94bf5ac1b97f49957940170256abb3ab9128796
RocWang <rocwangi@sina.cn>
2018-04-20 16:26:39 +0800

    添加loader对less和其他静态资源支持

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var path = require('path');
+var _ = require('lodash');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
@@ -9,9 +10,22 @@ var index = require('./routes/index');
 var users = require('./routes/users');
 var ejs = require('ejs');
 var expressLayouts = require('express-ejs-layouts');
-
+var Loader = require('loader');
+var LoaderConnect = require('loader-connect')
 var app = express();
 
+var config = require('./config');
+
+var assets = {};
+
+if (config.mini_assets) {
+    try {
+        assets = require('./assets.json');
+    } catch (e) {
+        console.warn('You must execute `npm run build` before start app when mini_assets is true.');
+        throw e;
+    }
+}
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.engine('html', ejs.__express);
@@ -23,8 +37,13 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 app.use(expressLayouts);
+
+// 静态资源
+if (config.debug) {
+    app.use(LoaderConnect.less(__dirname)); // 测试环境用，编译 .less on the fly
+}
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
@@ -34,6 +53,12 @@ app.use(function (req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
+});
+
+_.extend(app.locals, {
+    config: config,
+    Loader: Loader,
+    assets:assets
 });
 
 // error handler
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var config = require('../config');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', { title: 'Express', config: config });
 });
 
 module.exports = router;
